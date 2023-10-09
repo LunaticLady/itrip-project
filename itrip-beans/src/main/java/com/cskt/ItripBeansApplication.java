3954fcfc1e6959b8f60a4aa6cea0d4a17644f384
zdjzdj <1936387425@qq.com>
2023-10-09 15:29:56 +0800

    dev

--- itrip-beans/src/main/java/com/cskt/ItripBeansApplication.java
+++ itrip-beans/src/main/java/com/cskt/ItripBeansApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItripBeansApplication {
     public static void main(String[] args) {
-        System.out.println("master分支提交");
         SpringApplication.run(ItripBeansApplication.class, args);
     }
 }
